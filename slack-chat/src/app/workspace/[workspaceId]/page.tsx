46c143a3754081515face0cc37e2a0103ff41020
sourav <souravm.mitra@gmail.com>
2025-07-02 19:08:36 +0530

    feat: global search on channels and members using command package

--- slack-chat/src/app/workspace/[workspaceId]/page.tsx
+++ slack-chat/src/app/workspace/[workspaceId]/page.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 
 import { useGetChannels } from "@/features/channels/api/use-get-channels";
 import { useCreateChannelModal } from "@/features/channels/store/use-create-channel-modal";
@@ -10,97 +10,110 @@ import { useRouter } from "next/navigation";
 import { useEffect, useMemo } from "react";
 
 const WorkSpaceIdPage = () => {
+  const router = useRouter(); //For navigation..
+  const workspaceId = useWorkSpaceId(); //curr workspaceId
 
-    
-   const router = useRouter(); //For navigation..
-   const workspaceId = useWorkSpaceId();  //curr workspaceId
+  //NOTE: If the user deletes all Channels, --> We want the user to add atleast one channel..
+  const [open, setOpen] = useCreateChannelModal();
 
-   //NOTE: If the user deletes all Channels, --> We want the user to add atleast one channel..
-   const[open, setOpen] = useCreateChannelModal();
+  //fetch curr workspace details...
+  const { data: workspaceData, isLoading: isWorkspaceLoading } =
+    useGetByIdWorkspace({
+      id: workspaceId,
+    });
+  //fetch member details...
+  const { data: currMember, isLoading: isCurrMemberLoading } = useCurrentMember(
+    {
+      id: workspaceId,
+    }
+  );
+  //fetch current channel data..
+  const { data: channelData, isLoading: isChannelLoading } = useGetChannels({
+    workspaceId,
+  });
 
-   //fetch curr workspace details...
-   const{data: workspaceData, isLoading:isWorkspaceLoading} = useGetByIdWorkspace({
-    id: workspaceId
-   })
-   //fetch member details...
-   const{data: currMember, isLoading: isCurrMemberLoading} = useCurrentMember({
-    id:workspaceId
-   })
-   //fetch current channel data..
-   const{data: channelData, isLoading: isChannelLoading} = useGetChannels({
-    workspaceId
-   })
+  //It will memorise the first channelId it can get...
+  const channelId = useMemo(() => channelData?.[0]?._id, [channelData]);
+  const isAdmin = useMemo(
+    () => currMember?.role === "admin",
+    [currMember?.role]
+  );
 
-   //It will memorise the first channelId it can get...
-   const channelId = useMemo(()=>channelData?.[0]?._id, [channelData]);
-   const isAdmin = useMemo(() => currMember?.role === 'admin', [currMember?.role]);
+  console.log("##workpace", workspaceData);
+  console.log("##isAdmin", isAdmin);
+  console.log("##ChannelId", channelId);
 
-   console.log('##workpace', workspaceData);
-   console.log('##isAdmin', isAdmin);
-   console.log('##ChannelId', channelId);
-   
-
-   
-   //This Handles the naviagtion to the first channel if it finds --> If not -> Opens the create New channel Modal.
-   //NOTE: We are dealing with a lot of state dependent variable inside useEffect here --> added them in the dependency arr.
-   useEffect(()=>{
+  //This Handles the naviagtion to the first channel if it finds --> If not -> Opens the create New channel Modal.
+  //NOTE: We are dealing with a lot of state dependent variable inside useEffect here --> added them in the dependency arr.
+  useEffect(() => {
     //If the data is still loading...
-    if(isWorkspaceLoading || isChannelLoading || isCurrMemberLoading || !workspaceData)
-        return;
+    if (
+      isWorkspaceLoading ||
+      isChannelLoading ||
+      isCurrMemberLoading ||
+      !workspaceData
+    )
+      return;
     //If you find the first channel Id for that workspace...
-    if(channelId) {
-        router.push(`/workspace/${workspaceId}/channel/${channelId}`);  //Naviagtes to the channel Id ...
-    } else if(!open && isAdmin) {
-        setOpen(true);  //If no Channel Found --> Open the create Channel Popup.. [Only if the member is an Admin]
+    if (channelId) {
+      router.push(`/workspace/${workspaceId}/channel/${channelId}`); //Naviagtes to the channel Id ...
+    } else if (!open && isAdmin) {
+      setOpen(true); //If no Channel Found --> Open the create Channel Popup.. [Only if the member is an Admin]
     }
-   }, [
-        channelId,
-        isWorkspaceLoading,
-        isChannelLoading,
-        workspaceData,
-        router,
-        open,
-        setOpen,
-        workspaceId
-    ])
-  
-    //If Workpsace or Channel is loading...
-    if(isWorkspaceLoading || isChannelLoading || isCurrMemberLoading) {
-        return (
-            <div className="h-full flex-1 flex justify-center items-center flex-col gap-2">
-                <Loader className="size-8 animate-spin text-muted-foreground" />
-            </div>
-        )
-    }
+  }, [
+    channelId,
+    isWorkspaceLoading,
+    isChannelLoading,
+    workspaceData,
+    router,
+    open,
+    setOpen,
+    workspaceId,
+    isCurrMemberLoading,
+    isAdmin,
+  ]);
 
-    //If the workspace itself doesnot exist. case -> If we are a member and workspace got deleted..
-    if(!workspaceData || !currMember) {
-        return (
-            <div className="h-full flex-1 flex justify-center items-center flex-col gap-2">
-                <TriangleAlert className="size-8 text-rose-500" />
-                <span className="text-sm text-muted-foreground">Workspace not found</span>
-            </div>
-        )
-    }
+  //If Workpsace or Channel is loading...
+  if (isWorkspaceLoading || isChannelLoading || isCurrMemberLoading) {
+    return (
+      <div className="h-full flex-1 flex justify-center items-center flex-col gap-2">
+        <Loader className="size-8 animate-spin text-muted-foreground" />
+      </div>
+    );
+  }
 
-    if(!isAdmin) {
-        return (
-            <div className="h-full flex-1 flex justify-center items-center flex-col gap-2">
-                <TriangleAlert className="size-8 text-rose-500" />
-                <span className="text-sm text-muted-foreground">Channel not found Or Not Selected</span>
-            </div>
-        )
-    }
+  //If the workspace itself doesnot exist. case -> If we are a member and workspace got deleted..
+  if (!workspaceData || !currMember) {
+    return (
+      <div className="h-full flex-1 flex justify-center items-center flex-col gap-2">
+        <TriangleAlert className="size-8 text-rose-500" />
+        <span className="text-sm text-muted-foreground">
+          Workspace not found
+        </span>
+      </div>
+    );
+  }
 
-    /*
+  if (!isAdmin) {
+    return (
+      <div className="h-full flex-1 flex justify-center items-center flex-col gap-2">
+        <TriangleAlert className="size-8 text-rose-500" />
+        <span className="text-sm text-muted-foreground">
+          Channel not found Or Not Selected
+        </span>
+      </div>
+    );
+  }
+
+  /*
         At this point, we have handled most of the cases.
             1. If the channelId exisit --> It will got to the respective channel Page.
             2. If the channel and workspace data loading --> show a loading icon
             3. If the workspace doesnt exist --> Show an error message saying workspace doesnt exist
             4. If the current member is not an Admin and he has no other channel in that workspace -> Show a proper error message.
     */
-   
-    return null;
-}
+
+  return null;
+};
 
 export default WorkSpaceIdPage;
--- slack-chat/src/app/workspace/[workspaceId]/toolbar.tsx
+++ slack-chat/src/app/workspace/[workspaceId]/toolbar.tsx
@@ -1,39 +1,102 @@
 "use client"; //For that useParam used in useWorkspaceId hook, layout will mark as client render..
 
-import { Button } from "@/components/ui/button"
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { Button } from "@/components/ui/button";
 import { useGetByIdWorkspace } from "@/features/workspaces/api/use-getById-workspaces";
-import { useWorkSpaceId } from "@/hooks/use-workspace-id"
-import { Armchair, Info, Search } from "lucide-react"
+import { useWorkSpaceId } from "@/hooks/use-workspace-id";
+import { Armchair, Info, Search } from "lucide-react";
+import {
+  CommandDialog,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+  CommandSeparator,
+} from "@/components/ui/command";
+import { useGetChannels } from "@/features/channels/api/use-get-channels";
+import { useGetMembers } from "@/features/members/api/use-get-members";
+import { Id } from "../../../../convex/_generated/dataModel";
 
 export const Toolbar = () => {
-    const id = useWorkSpaceId();
-    const { data, isLoading } = useGetByIdWorkspace({id});
-    return (
+  const id = useWorkSpaceId();
+  const [open, setOpen] = useState<boolean>(false);
+  const router = useRouter();
+
+  const { data, isLoading } = useGetByIdWorkspace({ id });
+  const { data: channels } = useGetChannels({ workspaceId: id });
+  const { data: members } = useGetMembers({ workspaceId: id });
+
+  // handles on click of channel id from search...
+  const handleChannelClick = (channelId: Id<"channels">) => {
+    setOpen(false);
+    router.push(`/workspace/${id}/channel/${channelId}`);
+  };
+
+  // handles on click of member from search...
+  const handleMemberClick = (memberId: Id<"members">) => {
+    setOpen(false);
+    router.push(`/workspace/${id}/members/${memberId}`);
+  };
+
+  return (
     <nav className="bg-[rgb(72,19,73)] flex item-center justify-between h-10 p-1.5">
-        {/* This covers the left side empty space of the toolbar */}
-        <div className="flex flex-1 space-x-3 justify-start items-center" >
-            <Armchair className="size-9 text-orange-300 ml-3" />
-            <div className="text-xl font-semibold text-white">
-                Slack Couch
-            </div>
+      {/* This covers the left side empty space of the toolbar */}
+      <div className="flex flex-1 space-x-3 justify-start items-center">
+        <Armchair className="size-9 text-orange-300 ml-3" />
+        <div className="flex item-center text-xl font-semibold text-white">
+          Slack Couch
         </div>
-        
-        {/* The search box bar */}
-        <div className="min-w-[400px] max-w-[642px] grow-[2] shrink">
-            <Button size="sm" className="bg-accent/25 hover:bg-accent/25 w-full flex justify-start h-7 px-2">
-                <Search className="size-4 text-white mr-2" />
-                <span className="text-white text-xs">
-                    Search in {isLoading ? 'Workspace' : data?.name}
-                </span>
-            </Button>
-        </div>
-        
-        {/* The info Icon Right side Toolbar */}
-        <div className="ml-auto flex flex-1 items-center justify-end">
-            <Button variant='transparent' size='iconSm'>
-                <Info className="size-5 text-white" />
-            </Button>
-        </div>
+      </div>
+
+      {/* The search box bar */}
+      <div className="min-w-[400px] max-w-[642px] grow-[2] shrink">
+        <Button
+          size="sm"
+          className="bg-accent/25 hover:bg-accent/25 w-full flex justify-start h-7 px-2"
+          onClick={() => setOpen(true)}
+        >
+          <Search className="size-4 text-white mr-2" />
+          <span className="text-white text-xs">
+            Search in {isLoading ? "Workspace" : data?.name}
+          </span>
+        </Button>
+        <CommandDialog open={open} onOpenChange={setOpen}>
+          <CommandInput placeholder="Type a command or search..." />
+          <CommandList>
+            <CommandEmpty>No results found.</CommandEmpty>
+            <CommandGroup heading="Channels">
+              {channels?.map((channel) => (
+                <CommandItem
+                  key={channel._id}
+                  onSelect={() => handleChannelClick(channel._id)}
+                >
+                  # {channel.name}
+                </CommandItem>
+              ))}
+            </CommandGroup>
+            <CommandSeparator />
+            <CommandGroup heading="Members">
+              {members?.map((member) => (
+                <CommandItem
+                  key={member._id}
+                  onSelect={() => handleMemberClick(member._id)}
+                >
+                  {member.user.name}
+                </CommandItem>
+              ))}
+            </CommandGroup>
+          </CommandList>
+        </CommandDialog>
+      </div>
+
+      {/* The info Icon Right side Toolbar */}
+      <div className="ml-auto flex flex-1 items-center justify-end">
+        <Button variant="transparent" size="iconSm">
+          <Info className="size-5 text-white" />
+        </Button>
+      </div>
     </nav>
-    )
-}
+  );
+};
